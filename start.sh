9d0501b3b6fe8ba058e6c9e3641a70ab2eae0b33
Raphael Parree <rparree@edc4it.com>
2015-10-19 08:35:36 +0200

    exposing ports used on fabric demo

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "removing previous fuse container (if any)"
 docker rm -f fuse
 echo "Starting"
-docker run -Pd -p 8101:8101 \
+docker run -Pd -p 8101:8101 -p 61616:61616 \
   --name fuse --link nexus \
     rparree/jboss-fuse-full-admin
 docker port fuse
