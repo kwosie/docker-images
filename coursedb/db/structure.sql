907627f206b45cb63369779f9ab918a2f6123652
Raphael Parree <rparree@edc4it.com>
2017-05-21 12:12:00 +0200

    [coursedb] removed some constraint

--- coursedb/db/structure.sql
+++ coursedb/db/structure.sql
@@ -52,27 +52,26 @@ CREATE TABLE city (
 CREATE TABLE country (
     code character(3) NOT NULL,
     name text NOT NULL,
-    continent text NOT NULL,
-    region text NOT NULL,
-    surfacearea real NOT NULL,
+    continent text ,
+    region text ,
+    surfacearea real ,
     indepyear smallint,
-    population integer NOT NULL,
+    population integer ,
     lifeexpectancy real,
     gnp numeric(10,2),
     gnpold numeric(10,2),
-    localname text NOT NULL,
-    governmentform text NOT NULL,
+    localname text ,
+    governmentform text ,
     headofstate text,
     capital integer,
-    code2 character(2) NOT NULL,
-    CONSTRAINT country_continent_check CHECK ((((((((continent = 'Asia'::text) OR (continent = 'Europe'::text)) OR (continent = 'North America'::text)) OR (continent = 'Africa'::text)) OR (continent = 'Oceania'::text)) OR (continent = 'Antarctica'::text)) OR (continent = 'South America'::text)))
+    code2 character(2) 
 );
 
 CREATE TABLE countrylanguage (
     countrycode character(3) NOT NULL,
     "language" text NOT NULL,
-    isofficial boolean NOT NULL,
-    percentage real NOT NULL
+    isofficial boolean ,
+    percentage real 
 );
 
 COPY city (id, name, countrycode, district, population) FROM stdin;
